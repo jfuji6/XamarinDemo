8b7cd4d21bbb6d48d6a39bc76f4ce53e2ac1c9f6
jfuji6 <fuji1@cox.net>
2015-11-23 12:52:59 -0800

    test

---
+++ XamarinDemo/Droid/Data/SQLite_Android.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using XamarinDemo;
+using XamarinDemo.Droid;
+
+[assembly: Dependency (typeof (SQLite_Android))]
+namespace XamarinDemo.Droid
+{
+	public class SQLite_Android : ISQLite
+	{
+		public SQLite_Android ()
+		{
+		}
+
+		public SQLite.SQLiteConnection GetConnection()
+		{
+			const string sqliteFilename = "XamarinDemo.db3";
+			string documentsPath = System.Environment.GetFolderPath 
+				(System.Environment.SpecialFolder.Personal);
+
+			var path = Path.Combine (documentsPath, sqliteFilename);
+
+			var conn = new SQLite.SQLiteConnection (path);
+			return conn;
+		}
+	}
+}
+
